c30f6e962c82f8f6aeb292a670842db7d43b2b7b
Mert <95344086+mertks00@users.noreply.github.com>
2022-05-06 01:44:29 +0300

    Update ChatManager.java

--- src/main/java/me/chatmanager/chatmanager/ChatManager.java
+++ src/main/java/me/chatmanager/chatmanager/ChatManager.java
@@ -1,41 +1,34 @@
-package me.chatmanager.chatmanager;
+package me.clear.clear;
 
 import net.md_5.bungee.api.ChatColor;
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class ChatManager extends JavaPlugin {
-
-    @Override
-    public void onEnable() {
-        // Plugin startup logic
-
-    }
+public final class Clear extends JavaPlugin {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 
-        if (command.getName().equalsIgnoreCase("cc")) {
-
+        if (command.getName().equalsIgnoreCase("sil")) {
             if (sender instanceof Player) {
                 Player player = (Player) sender;
-                if (player.hasPermission("ChatManager.admin")) {
+                    if (player.hasPermission("clear.admin")) {
 
-                    String message = " ";
-                    for (int i = 0; i < 20; i++) {
-                        player.sendMessage(" ");
+                        for (int i = 0; i< 20; i++) {
+                            Bukkit.broadcastMessage(" ");
+
+                        }
+                        Bukkit.broadcastMessage(ChatColor.GREEN+"Sohbet "+ ChatColor.YELLOW+player.getDisplayName()+ChatColor.GREEN+" Tarafından Temizlendi!");
+
+                    }else {
+                        player.sendMessage(ChatColor.RED+"Buna izniniz yok.");
                     }
-                    player.sendMessage(ChatColor.GREEN+"Sohbet "+ChatColor.YELLOW+player.getDisplayName()+ChatColor.GREEN+" Tarafından Temizlendi!");
 
-                }else {
-                    player.sendMessage(ChatColor.RED + "no permission");
-                }
 
             }
-
-
         }
 
         return true;
